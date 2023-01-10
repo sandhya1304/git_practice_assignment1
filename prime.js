e25ce266e7f8e10d310213a9d367a47b8efd0515
sandhya <reply2sandhyaghumare5@gmail.com>
2023-01-10 21:17:18 +0530

    i will checkout it

--- prime.js
+++ prime.js
@@ -1,11 +1,10 @@
-let n=13;
-let count=0;
-for(let i=0;i<n.length;i++){
-if(n%i==1){
-}
-}
-if(count==0){
-console.log("Yes");
-}else{
-console.log("No");
-}
+for(let i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+    if(count==0){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
